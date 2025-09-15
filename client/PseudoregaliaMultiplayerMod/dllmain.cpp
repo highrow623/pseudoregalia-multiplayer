81c94ebde90918fa5abfe2973edc225db998ee02
highrow623 <highrow623@gmail.com>
2025-09-14 21:46:24 -0600

    added player info struct

--- client/PseudoregaliaMultiplayerMod/dllmain.cpp
+++ client/PseudoregaliaMultiplayerMod/dllmain.cpp
@@ -3,7 +3,10 @@
 
 #include "Unreal/AActor.hpp"
 #include "Unreal/Hooks.hpp"
+#include "Unreal/TArray.hpp"
 #include "Unreal/UClass.hpp"
+
+#include "ST_PlayerInfo.hpp"
 
 class PseudoregaliaMultiplayerMod : public RC::CppUserModBase
 {
@@ -33,18 +36,19 @@ public:
         {
             if (actor->GetClassPrivate()->GetName() == L"BP_PM_Manager_C")
             {
-                // TODO
-                // OnSceneLoad();
+                // TODO handle connection based on level? if non-gameplay level, close connection if one exists; if
+                // gameplay level, establish connection if one doesn't exist
+                // Client::OnSceneLoad(actor->GetLevel()->GetName());
 
                 if (!sync_items_hooked)
                 {
-                    RC::Unreal::UFunction* func = actor->GetFunctionByName(L"SyncItems");
+                    RC::Unreal::UFunction* func = actor->GetFunctionByName(L"SyncInfo");
                     if (!func)
                     {
                         // TODO log error
                         return;
                     }
-                    RC::Unreal::UObjectGlobals::RegisterHook(func, sync_items, nop, nullptr);
+                    RC::Unreal::UObjectGlobals::RegisterHook(func, sync_info, nop, nullptr);
                     sync_items_hooked = true;
                 }
             }
@@ -53,7 +57,7 @@ public:
 
     auto on_update() -> void override
     {
-        // TODO
+        // TODO poll server and potentially send messages based on current player info
         // Client::Tick();
     }
 
@@ -61,9 +65,15 @@ public:
     {
     }
 
-    static void sync_items(RC::Unreal::UnrealScriptFunctionCallableContext& context, void* customdata)
+    static void sync_info(RC::Unreal::UnrealScriptFunctionCallableContext& context, void* customdata)
     {
-        // TODO
+        auto& player_info = context.GetParams<FST_PlayerInfo>();
+        // TODO use player_info to set info for sending to server
+        // Client::SetPlayerInfo(player_info);
+        RC::Unreal::TArray<FST_PlayerInfo> ghost_info{};
+        // TODO get ghost_info
+        // Client::GetGhostInfo(ghost_info);
+        context.SetReturnValue(ghost_info);
     }
 };
 
