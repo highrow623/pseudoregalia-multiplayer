e18a91a88862369c3830217c60ab5f96d583220d
highrow623 <highrow623@gmail.com>
2025-09-17 00:16:14 -0600

    added sybil model to ghost

--- client/PseudoregaliaMultiplayerMod/include/ST_PlayerInfo.hpp
+++ client/PseudoregaliaMultiplayerMod/include/ST_PlayerInfo.hpp
@@ -2,13 +2,13 @@
 #define UE4SS_SDK_ST_PlayerInfo_HPP
 
 #include "Unreal/FString.hpp"
-#include "Unreal/UnrealCoreStructs.hpp"
+#include "Unreal/Transform.hpp"
 
 struct FST_PlayerInfo
 {
-    RC::Unreal::FString id_5_ACEE325C40A82A6010DF82BF448FD0B6;       // 0x0000 (size: 0x10)
-    RC::Unreal::FString zone_2_7A1ED7FA4276A703DD7058A44D2AF8DA;     // 0x0010 (size: 0x10)
-    RC::Unreal::FVector location_8_A99B33C64A7E3258D121F292594BE28D; // 0x0020 (size: 0x18)
-}; // Size: 0x38
+    RC::Unreal::FString id_5_ACEE325C40A82A6010DF82BF448FD0B6;            // 0x0000 (size: 0x10)
+    RC::Unreal::FString zone_2_7A1ED7FA4276A703DD7058A44D2AF8DA;          // 0x0010 (size: 0x10)
+    RC::Unreal::FTransform transform_10_A99B33C64A7E3258D121F292594BE28D; // 0x0020 (size: 0x60)
+}; // Size: 0x80
 
 #endif
