50869ce6cdc0e33e5f53c3487e7091ef035a7a3e
highrow623 <highrow623@gmail.com>
2025-09-25 21:17:49 -0600

    ignore empty command

--- server/src/serve/stdin.rs
+++ server/src/serve/stdin.rs
@@ -8,9 +8,12 @@ pub fn handle_command(_state: &Arc<Mutex<State>>, command: &str) {
     // We don't use state yet but it could theoretically be used for all sorts of things
     // e.g. a /warp_all command, which would send a message to all clients
     // or a command to get info about currently connected players
-    if command == "/exit" {
-        println!("terminating server");
-        process::exit(0);
+    match command {
+        "/exit" => {
+            println!("terminating server");
+            process::exit(0);
+        }
+        "" => {}
+        _ => println!("unrecognized command: {}", command),
     }
-    println!("unrecognized command: {}", command);
 }
